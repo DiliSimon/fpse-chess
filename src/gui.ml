725a5185eeffa32be81ee66314ea002c44b6972b
TianyueOu <tou3@jhu.edu>
2021-12-14 22:02:09 -0500

    make printed board prettier

--- src/gui.ml
+++ src/gui.ml
@@ -2,26 +2,26 @@ open Board
 open Core
 
 let print_board (b:board) (p:player) =
-  print_string " 01234567\n";
+  print_string " 0 1 2 3 4 5 6 7\n";
   List.iteri b ~f:(
     fun i row ->
       print_string @@ Int.to_string i;
       List.iter row ~f:(
       fun piece ->
       (match piece with
-      | Empty -> " "
-      | Occupied(King(White,_)) -> "♔"
-      | Occupied(Queen(White)) -> "♕"
-      | Occupied(Rook(White,_)) -> "♖"
-      | Occupied(Bishop(White)) -> "♗"
-      | Occupied(Knight(White)) -> "♘"
-      | Occupied(Pawn(White)) -> "♙"
-      | Occupied(King(Black,_)) -> "♚"
-      | Occupied(Queen(Black)) -> "♛"
-      | Occupied(Rook(Black,_)) -> "♜"
-      | Occupied(Bishop(Black)) -> "♝"
-      | Occupied(Knight(Black)) -> "♞"
-      | Occupied(Pawn(Black)) -> "♟")
+      | Empty -> "  "
+      | Occupied(King(White,_)) -> "♔ "
+      | Occupied(Queen(White)) -> "♕ "
+      | Occupied(Rook(White,_)) -> "♖ "
+      | Occupied(Bishop(White)) -> "♗ "
+      | Occupied(Knight(White)) -> "♘ "
+      | Occupied(Pawn(White)) -> "♙ "
+      | Occupied(King(Black,_)) -> "♚ "
+      | Occupied(Queen(Black)) -> "♛ "
+      | Occupied(Rook(Black,_)) -> "♜ "
+      | Occupied(Bishop(Black)) -> "♝ "
+      | Occupied(Knight(Black)) -> "♞ "
+      | Occupied(Pawn(Black)) -> "♟ ")
       |> print_string
     );
     print_string "\n"
